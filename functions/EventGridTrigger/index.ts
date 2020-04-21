a85bd5242db99545cbb8aac8fa7850999a41309b
Kazumi OHIRA <dzeyelid@gmail.com>
2020-04-21 23:48:14 +0900

    Update function's code for test.

--- functions/EventGridTrigger/index.ts
+++ functions/EventGridTrigger/index.ts
@@ -1,6 +1,7 @@
 import { AzureFunction, Context } from "@azure/functions"
 
 const eventGridTrigger: AzureFunction = async function (context: Context, eventGridEvent: any): Promise<void> {
+    // TODO: implement
     context.log(typeof eventGridEvent);
     context.log(eventGridEvent);
 };
